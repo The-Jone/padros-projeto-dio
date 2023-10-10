be8f3bf5ae179fc5045020a965208225d5dc10dd
Jone <jone12271620@gmail.com>
2023-10-10 12:55:02 -0400

    padroes-projeto

---
+++ padros-projeto/src/digitalinnovation/Strategy/ComportamentoDefensivo.java
@@ -0,0 +1,10 @@
+package digitalinnovation.Strategy;
+
+public class ComportamentoDefensivo implements Comportamento {
+
+	@Override
+	public void mover() {
+		System.out.println("Movendo-se defensivamente...");
+	}
+
+}
---
+++ padros-projeto/src/digitalinnovation/Strategy/ComportamentoNormal.java
@@ -0,0 +1,10 @@
+package digitalinnovation.Strategy;
+
+public class ComportamentoNormal implements Comportamento {
+
+	@Override
+	public void mover() {
+		System.out.println("Movendo-se normalmente...");
+	}
+
+}
